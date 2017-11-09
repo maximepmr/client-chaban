b9fdbc69c982214e4c61ea226e316da3811b0862
maxim <maxime.pomier@supersoluce.com>
2017-11-09 22:37:01 +0100

    comment

--- src/pages/home.js
+++ src/pages/home.js
@@ -24,11 +24,13 @@ class HomePage extends Component {
     handleSubmit(event) {
         const target = event.target;
 
+        var incorrect = 0;
         var dateFormat = require('dateformat');
 
         // variable contenant les paramètres
         var params = '';
 
+        // on test les valeurs pour incrémenter param qui contient les  paramètre de la recherche
         if (target.from.value) {
             var from = new Date(target.from.value);
             params = '?from=' + dateFormat(from, "dd-mm-yy");
@@ -43,17 +45,31 @@ class HomePage extends Component {
             params += '&to=';
         }
 
-        alert(params);
-        fetch('http://localhost:1337/' + params)
-            .then((res) => res.json())
-            // use parsed response
-            .then((json) => {
-                this.setState({
-                    data: json,
-                });
-            }).catch(function() {
-            alert("OUPS le serveur ne répond pas, nous allons réesayer dans quelques secondes");
-        })
+        // on vérifie que From est bien inférieur à to sinon on prévient l'utilisateur de l'erreur
+        if ((target.to.value)&&(target.from.value)){
+            var dateFrom = new Date(target.from.value);
+            var dateTo = new Date(target.to.value);
+
+            if (dateTo < dateFrom) {
+                incorrect = 1;
+            }
+        }
+        // si la recherche est bonne alors on exécute la recherche directement sur l'api sinon on transmet une alerte à l'utilisateur
+        if (!incorrect) {
+            fetch('http://localhost:1337/' + params)
+                .then((res) => res.json())
+                // use parsed response
+                .then((json) => {
+                    this.setState({
+                        data: json,
+                    });
+                }).catch(function() {
+                alert("OUPS le serveur ne répond pas, nous allons réesayer dans quelques secondes");
+            })
+        } else {
+
+            alert('Erreur, To est inférieur à From');
+        }
         event.preventDefault();
     }
 
